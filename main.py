c6c6bf1a648fac93eb082d460ad811378a9fa72f
Armin Berger <armin@bergr.ch>
2023-04-05 10:42:21 +0200

    bugfix

--- main.py
+++ main.py
@@ -10,24 +10,24 @@ def get_paths():
         model_path = sys.argv[3]
     except IndexError:
         print('Please provide the path to the project root, data folder and model (in this order)')
-        return None, None
+        return None, None, None
 
     if not os.path.exists(project_root_path):
         print('The path to the project root does not exist')
-        return None, None
+        return None, None, None
     if not os.path.exists(data_path):
         print('The path to the data folder does not exist')
-        return None, None
+        return None, None, None
     if not os.path.exists(model_path):
         print('The path to the model folder does not exist')
-        return None, None
+        return None, None, None
 
     print(project_root_path)
     print(data_path)
-    return project_root_path, data_path
+    return project_root_path, data_path, model_path
 
 def main():
-    project_root_path, data_path = get_paths()
+    project_root_path, data_path, model_path = get_paths()
     if project_root_path is None or data_path is None:
         return
 
